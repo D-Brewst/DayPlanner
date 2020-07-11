3d1526749bb9f59b50a289ae6549393a31f499c4
D-Brewst <devonbrewster31@gmail.com>
2020-07-11 02:49:05 -0400

    Added localStorage functionality

--- script.js
+++ script.js
@@ -2,22 +2,28 @@
 var today = $("#currentDay");
 var editDiv = $(".editDiv");
 var Btns = $(".saveBtn");
-var content = [];
 var input = $(".MyInput");
+var listTimes = ["8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM"];
 
-console.log(input[0])
 // Setting current date
 var m = moment();
 today.text(m.format("dddd MMMM Do YYYY"));
 
-// Adding event listener to all buttons
-for(var i = 0; i < Btns.length; i++){
-    Btns.on("click", saveInputs);
+for(var i = 0; i < listTimes.length; i++){
+    $(`#t${i} .MyInput`).val(localStorage.getItem(listTimes[i]));
 }
 
-function saveInputs(event){
-     content.push(input)   
+// Adding event listener to all buttons
+Btns.on("click", saveInputs);
+
+function saveInputs(){
+    var parent = $(this).parent();
+    var sibling = parent.siblings();
+    var inputBox = sibling.children(".MyInput").val();
+    var apptTime = sibling.children(".clock").html();
+    localStorage.setItem(apptTime, inputBox);
 }
+
 
 // Detecting user input in editDivs
 // let observer = new MutationObserver(mutations =>
