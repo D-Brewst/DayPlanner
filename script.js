db9411676d96aad8cead4b52ff243ed812b0d554
D-Brewst <devonbrewster31@gmail.com>
2020-07-11 06:22:03 -0400

    Divs change colors based on current time

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Dependencies
 var today = $("#currentDay");
 var editDiv = $(".editDiv");
+var timeDivs = $(".time")
 var Btns = $(".saveBtn");
 var input = $(".MyInput");
 var listTimes = ["8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM"];
+var m = moment();
+var currentHour = parseInt(m.format("HH"));
+var currentMin = parseInt(m.format("mm"));
 
 // Setting current date
-var m = moment();
-today.text(m.format("dddd MMMM Do YYYY"));
+today.text(m.format("LLLL"));
 
+// Getting stored input values
 for(var i = 0; i < listTimes.length; i++){
     $(`#t${i} .MyInput`).val(localStorage.getItem(listTimes[i]));
 }
@@ -24,28 +28,29 @@ function saveInputs(){
     localStorage.setItem(apptTime, inputBox);
 }
 
+// Changing div styles based on current time.
+timeDivs.each(function(element){
+    var timecolumn = $(this);
+    var plannerTimes = parseInt(timecolumn.children(".clock").html().split(":")[0]);
+    var minuteTimes = parseInt(timecolumn.children(".clock").html().split(":")[1]);
+    if(plannerTimes === currentHour){
+        timecolumn.siblings(".editDiv").css("background-color", "red");
+        if(minuteTimes > currentMin){
+            timecolumn.siblings(".editDiv").css("background-color", "lawngreen");
+        }
+        if(minuteTimes < currentMin){
+            timecolumn.siblings(".editDiv").css("background-color", "gray");
+        }
+    }
+    if(plannerTimes > currentHour){
+        timecolumn.siblings(".editDiv").css("background-color", "lawngreen");
+    }
+    if(plannerTimes < currentHour){
+        timecolumn.siblings(".editDiv").css("background-color", "gray");
+    }
+})
 
-// Detecting user input in editDivs
-// let observer = new MutationObserver(mutations =>
-//     mutations.forEach(mutation => {
-//         console.log('input changed');
-//     })
-//     );
 
-// for(var i = 0; i < editDiv.length; i++){
-//     observer.observe(editDiv[i], {
-//     childList: true,
-//     characterData: true,
-//     subtree: true,
-//   });
-// }
+// if(currentTime > )
 
-// Get text from editDiv
-function getText(event){
-    for(var i = 0; i < editDiv.length; i++){
-        content.push(editDiv[i].innerHTML);
-        localStorage.setItem("appointments", JSON.stringify(content));
-    }   
-    JSON.parse(localStorage.getItem(content));
-}
 
